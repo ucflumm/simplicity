d971d1b7abd1563a0ad8be2fe85a1426d5bb2918
ucflumm <ucflumm@gmail.com>
2024-02-14 14:49:36 +1100

    Update item routes to include "id" in the URL

--- backend_api/app/routes/item.routes.js
+++ backend_api/app/routes/item.routes.js
@@ -8,23 +8,23 @@
   // Retrieve all Items
   router.get("/", item.findAll);
   // Retrieve a single Item with id
-  router.get("/:id", item.findOneById);
+  router.get("/id/:id", item.findOneById);
   // Retrieve a single Item with param and value
   router.get("/param/:param/value/:value", item.findbyParams);
   //Find all zero quantity items
   router.get("/zero", item.findAllZeroQuantity);
   // Update a Item with id by using the body of the request.
-  router.put("/:id", item.update);
+  router.put("/id/:id", item.update);
   // Update a Item's quantity with id
-  router.put("/:id/quantity/:quantity", item.updateQuantity);
+  router.put("/id/:id/quantity/:quantity", item.updateQuantity);
   // Update a Param via id
-  router.put("/:id/param/:param/value/:value", item.updateParamById);
+  router.put("/id/:id/param/:param/value/:value", item.updateParamById);
   //update quantity via upc
   router.put("/upc/:upc/quantity/:quantity", item.updateQuantityByUPC);
 
 
   // Delete a Item with id
-  router.delete("/:id", item.delete);
+  router.delete("/id/:id", item.delete);
 
   app.use('/api/item', router);
 }
