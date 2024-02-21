156095a9de825f1e90a7eb4c0c5c03da88736cad
EsmeraldaMar <6580@holbertonstudents.com>
2024-02-21 21:40:56 +1100

    added create item page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,51 +5,53 @@ import Sidenav from './SideNav'; // Import the Sidenav component
 import Header from './Header'; // Import the Header component
 import ProductGrid from './ProductGrid';
 import WhiteContainer from './WhiteContainer';
+import CreateItem from './CreateItem';
 // Import other components you might navigate to
 
 // Import your route components
 // import ItemLibraryComponent from './ItemLibraryComponent';
-// import CreateItemComponent from './CreateItemComponent';
+
 // import AdjustmentsComponent from './AdjustmentsComponent';
 
 
 const App = () => {
-    // const handleSearch = (searchTerm) => {
-    //   // add search logc here
-    //   console.log('Searching for:', searchTerm);
-    // };
-    return (
-        <Router>
-        <div className="App">
-            <Sidenav />
-            <Header />
-            <Routes>
-            <Route path="/" element={
+  // const handleSearch = (searchTerm) => {
+  //   // add search logc here
+  //   console.log('Searching for:', searchTerm);
+  // };
+  return (
+    <Router>
+      <div className="App">
+        <Sidenav />
+        <Header />
+        <Routes>
+          <Route path="/" element={
             <WhiteContainer>
-                <ProductGrid />
-            </WhiteContainer>
-            } />
-            {/* <Route path="/item-library" element={
-            <WhiteContainer>
-              <ItemLibraryComponent />
+              <ProductGrid />
             </WhiteContainer>
           } />
           <Route path="/create-item" element={
             <WhiteContainer>
-              <CreateItemComponent />
+              <CreateItem />
             </WhiteContainer>
           } />
+          {/* <Route path="/item-library" element={
+            <WhiteContainer>
+              <ItemLibraryComponent />
+            </WhiteContainer>
+          } />
+          
           <Route path="/adjustments" element={
             <WhiteContainer>
               <AdjustmentsComponent />
             </WhiteContainer>
           } /> */}
-            {/* Rest of your app */}
-            {/* Define more routes here */}
+          {/* Rest of your app */}
+          {/* Define more routes here */}
         </Routes>
-        </div>
-        </Router>
-    );
+      </div>
+    </Router>
+  );
 }
 
 export default App;
