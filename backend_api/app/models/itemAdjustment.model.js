7309a7df11c309e55f1f743961eb436555bebd77
Uwe Santos <ucflumm@gmail.com>
2024-03-10 23:01:17 +1100

    Item adjustment model (#36)
    * Add ItemAdjustment model to item.controller.js
    
    * Add ItemAdjustment model to item.controller.js
    
    * base changes
    
    * Refactor itemAdjustmentSchema in itemAdjustment.model.js
    
    * alpha version of trackItem!
    
    * Refactor item quantity update and tracking
    
    * beta version finish
    
    * beta version finish
    
    * fixed nesting in ItemID adjustments
    
    * removed refs
    
    * fixed uniq search adjustments
    
    * added delete all refs item
    
    * Remove unused code in adjustments.controller.js
    
    * Update response status codes in item controller

---
+++ backend_api/app/models/itemAdjustment.model.js
@@ -0,0 +1,33 @@
+//itenAdjustment.model.js
+const mongoose = require("mongoose");
+
+const itemAdjustmentSchema = new mongoose.Schema({
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Item",
+  },
+  upc: {
+    type: String,
+  },
+  user: {
+    type: String,
+  },
+  quantityChange: Number,
+  description: String,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Custom validation to ensure either itemId or upc is provided
+itemAdjustmentSchema.pre("validate", function (next) {
+  if (!this.itemId && !this.upc) {
+    this.invalidate("itemId", "Either an itemId or a UPC must be provided.");
+  }
+  next();
+});
+
+const ItemAdjustment = mongoose.model("ItemAdjustment", itemAdjustmentSchema);
+
+module.exports = ItemAdjustment;
