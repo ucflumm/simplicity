d4daf2240fe60caa805a85243b8596e4df0f69e2
EsmeraldaMar <gazpachobanana@gmail.com>
2024-03-14 10:40:32 +1100

    Feat: added styles for history table

--- frontend/src/HistoryTable.jsx
+++ frontend/src/HistoryTable.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
 
 const HistoryTable = ({ historyArray }) => {
-  console.log(historyArray);
   return (
     <TableContainer component={Paper}>
       <Table aria-label="history table">
         <TableHead>
           <TableRow>
-            <TableCell>Name (UPC)</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>User</TableCell>
-            <TableCell>Time</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '18px' }}>Name (UPC)</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '18px' }}>Description</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '18px' }}>User</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '18px' }}>Time</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {historyArray.map((entry) => (
-            <TableRow key={entry._id}>
-              <TableCell>{`${entry.name} (${entry.upc})`}</TableCell>
-              <TableCell>{entry.description}</TableCell>
-              <TableCell>{entry.user}</TableCell>
-              <TableCell>{new Date(entry.timestamp).toLocaleString()}</TableCell>
+          {historyArray.length > 0 ? (
+            historyArray.map((entry) => (
+              <TableRow key={entry._id}>
+                <TableCell style={{ fontSize: '16px' }}>{`${entry.name} (${entry.upc})`}</TableCell>
+                <TableCell style={{ fontSize: '16px' }}>{entry.description}</TableCell>
+                <TableCell style={{ fontSize: '16px' }}>{entry.user}</TableCell>
+                <TableCell style={{ fontSize: '16px' }}>{new Date(entry.timestamp).toLocaleString()}</TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={4} align="center">
+                <Typography variant="subtitle1" style={{ fontStyle: 'italic' }}>
+                  No history available
+                </Typography>
+              </TableCell>
             </TableRow>
-          ))}
+          )}
         </TableBody>
       </Table>
     </TableContainer>
