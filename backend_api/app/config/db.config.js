82f77ffb88b99d141ad2cbac9d191fd93fc0b7f5
ucflumm <ucflumm@gmail.com>
2024-03-19 00:04:30 +1100

    Update MongoDB connection URL in db.config.js

--- backend_api/app/config/db.config.js
+++ backend_api/app/config/db.config.js
@@ -1,3 +1,4 @@
+// In your db.config.js or similar, regardless of its location
 module.exports = {
-  url: "mongodb://mongodb:27017/sim_db"
+  url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongodb:27017/sim_db`,
 };
